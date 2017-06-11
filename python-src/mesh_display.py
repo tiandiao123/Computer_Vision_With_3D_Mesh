2ee0a9687b542bfc014062a1a3ea6534f1f0ccf7
Cuiqing Li <lixx3527@gmail.com>
2017-06-11 18:09:47 +0000

    add new file

---
+++ python-src/mesh_display.py
@@ -0,0 +1,16 @@
+import pymesh
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+from mayavi import mlab
+
+
+
+mesh = pymesh.load_mesh("target.ply")
+print(mesh.vertices)
+x=mesh.vertices[:,0]
+y=mesh.vertices[:,1]
+z=mesh.vertices[:,2]
+s = mlab.mesh(x, y, z)
+mlab.show()
