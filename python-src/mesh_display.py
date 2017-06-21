390de937b5938e201db83b950478e777312c2b4b
Cuiqing Li <lixx3527@gmail.com>
2017-06-21 16:32:25 -0400

    update mesh_display.py

--- python-src/mesh_display.py
+++ python-src/mesh_display.py
@@ -1,34 +1,19 @@
-import vtk
- 
-filename = "../PLY/target.py"
- 
-sphereSource = vtk.vtkSphereSource()
-sphereSource.Update()
- 
-plyWriter = vtk.vtkPLYWriter()
-plyWriter.SetFileName(filename)
-plyWriter.SetInputConnection(sphereSource.GetOutputPort())
-plyWriter.Write()
- 
-#Read and display for verication
-reader = vtk.vtkPLYReader()
-reader.SetFileName(filename)
-reader.Update()
- 
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(reader.GetOutputPort())
- 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
- 
-renderer = vtk.vtkRenderer()
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.AddRenderer(renderer)
-renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-renderWindowInteractor.SetRenderWindow(renderWindow)
- 
-renderer.AddActor(actor)
-renderer.SetBackground(.3, .6, .3)   #Background color green
- 
-renderWindow.Render()
-renderWindowInteractor.Start()
+from osgeo import gdal
+from tvtk.api import tvtk
+from mayavi import mlab
+import Image
+
+im1 = Image.open("../texture.jpg")
+im2 = im1.rotate(90)
+im2.save("tmp/ortofoto90.jpg")
+bmp1 = tvtk.JPEGReader(file_name="tmp/ortofoto90.jpg")
+
+my_texture=tvtk.Texture()
+my_texture.interpolate=0
+
+my_texture=tvtk.Texture(input_connection=bmp1.output_port, interpolate=0)
+surf=mlab.pipeline.surface(mlab.pipeline.open("PLY/target.ply"))
+surf.actor.enable_texture = True
+surf.actor.tcoord_generator_mode = 'plane'
+surf.actor.actor.texture = my_texture
+mlab.show()
